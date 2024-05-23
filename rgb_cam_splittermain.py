bc3b203010680e44938bc3649154887f43ae3496
JAYARAM <jram0012@github.com>
2024-05-23 18:42:00 +0530

    just tinkering with opencv

---
+++ rgb_cam_splittermain.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+ret = True
+cam = cv2.VideoCapture(0)
+outputframe = np.ndarray(shape=(480*2, 640*2, 3))
+
+org = (50, 50)
+font =  cv2.FONT_HERSHEY_SIMPLEX
+fontscale = 1
+thickness = 2
+
+while ret:
+    ret, frame = cam.read()
+    if not ret:
+        break
+
+    normalizedframe = frame / 255
+    
+    blue, green, red = cv2.split(normalizedframe)
+
+    red   = cv2.merge((red, red, red))
+    blue  = cv2.merge((blue, blue, blue))
+    green = cv2.merge((green, green, green))
+
+    red             = cv2.putText(red,             'red channel',    org, font, fontscale, (  0,   0, 255), thickness, cv2.LINE_AA)
+    green           = cv2.putText(green,           'green channel',  org, font, fontscale, (  0, 255,   0), thickness, cv2.LINE_AA)
+    blue            = cv2.putText(blue,            'blue channel',   org, font, fontscale, (255,   0,   0), thickness, cv2.LINE_AA)
+    normalizedframe = cv2.putText(normalizedframe, 'original frame', org, font, fontscale, (255, 255, 255), thickness, cv2.LINE_AA)
+
+    outputframe[480*0:480*1, 640*0:640*1, :] = normalizedframe
+    outputframe[480*0:480*1, 640*1:640*2, :] = red
+    outputframe[480*1:480*2, 640*0:640*1, :] = green
+    outputframe[480*1:480*2, 640*1:640*2, :] = blue
+
+
+    cv2.imshow('frame', outputframe)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
