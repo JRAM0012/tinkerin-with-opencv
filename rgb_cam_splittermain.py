4bd1fe537a515a2f2322022d252109714ed5fcf8
JAYARAM <jram0012@github.com>
2024-05-24 12:34:49 +0530

    some refactoring

--- rgb_cam_splittermain.py
+++ rgb_cam_splittermain.py
@@ -3,7 +3,11 @@ import numpy as np
 
 ret = True
 cam = cv2.VideoCapture(0)
-outputframe = np.ndarray(shape=(480*2, 640*2, 3))
+
+width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+outputframe = np.ndarray(shape=(height * 2, width * 2, 3))
 
 org = (50, 50)
 font =  cv2.FONT_HERSHEY_SIMPLEX
@@ -28,10 +32,10 @@ while ret:
     blue            = cv2.putText(blue,            'blue channel',   org, font, fontscale, (255,   0,   0), thickness, cv2.LINE_AA)
     normalizedframe = cv2.putText(normalizedframe, 'original frame', org, font, fontscale, (255, 255, 255), thickness, cv2.LINE_AA)
 
-    outputframe[480*0:480*1, 640*0:640*1, :] = normalizedframe
-    outputframe[480*0:480*1, 640*1:640*2, :] = red
-    outputframe[480*1:480*2, 640*0:640*1, :] = green
-    outputframe[480*1:480*2, 640*1:640*2, :] = blue
+    outputframe[height * 0:height * 1, width * 0:width * 1, :] = normalizedframe
+    outputframe[height * 0:height * 1, width * 1:width * 2, :] = red
+    outputframe[height * 1:height * 2, width * 0:width * 1, :] = green
+    outputframe[height * 1:height * 2, width * 1:width * 2, :] = blue
 
 
     cv2.imshow('frame', outputframe)
